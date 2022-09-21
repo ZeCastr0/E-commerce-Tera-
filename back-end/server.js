441f9abebfefc9dcea6cccfc22cbb284d8e3a200
José Inácio <103376174+ZeCastr0@users.noreply.github.com>
2022-09-20 22:07:56 -0300

    20/09/2022

--- back-end/server.js
+++ back-end/server.js
@@ -3,4 +3,4 @@ const PORT = process.env.PORT;
 
 // 3000, 6000, 6060, 8080, 8088
 
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
